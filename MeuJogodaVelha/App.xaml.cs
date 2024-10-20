ef888a4821dcf6cc6f9f0b2136f83363a52f544d
Marcos Henrique <marcosvilanova0000@gmail.com>
2024-10-19 22:57:43 -0300

    Primeiro Commit

---
+++ MeuJogodaVelha/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MeuJogodaVelha
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
